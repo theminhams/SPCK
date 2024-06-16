3aeaf7c2ceef146a529af87cc9fbc76aa3aae787
theminhams <theminhams@gmail.com>
2024-06-16 11:53:48 +0700

    fix

--- code/signinup/login.js
+++ code/signinup/login.js
@@ -17,7 +17,7 @@ buttonInput.addEventListener('click',
             const user = userCredential.user;
             // ...
             alert("Đăng nhập thành công")
-            window.location.href = "/project/code/index.html"
+            window.location.href = "/project/index.html"
         })
         .catch((error) => {
             const errorCode = error.code;
